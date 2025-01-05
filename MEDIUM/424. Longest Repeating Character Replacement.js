bf389e38430a87a7603617003a1ee0c37a32590a
Sxnny-s <ethsok@protonmail.com>
2025-01-05 10:09:53 -0500

    leet code easy

--- MEDIUM/424. Longest Repeating Character Replacement.js
+++ MEDIUM/424. Longest Repeating Character Replacement.js
@@ -23,11 +23,35 @@ var characterReplacement = function(s, k) {
     // 1.Two pointers l and r => slideing window (start at 0)
     // 2. We calculate the most common occurence of each letter in the window
     // 3. window length - most common <= k if so we have enough replacements to fill the wondow 
-    // 4. add that lenght to out nax variable
+    // 4. add that lenght to out max variable
     // 5. if not move the left pointer up  
     // 6. return max
 
+    let l = 0 
+    let hm = {}
+    let max = 0
+    let maxOccur = 0
+    for(let r = 0; r < s.length; r++){
+        let char = s[r]
+        hm[char] = (hm[char] || 0) + 1
+        maxOccur = Math.max(maxOccur, hm[char]);
+        // let maxOccur = Math.max(...Object.values(hm))
+        // let wL = Object.values(hm).reduce((a,c)=> a+c, 0)
+        let wL = r - l + 1
+        if(wL - maxOccur <= k){
+            max = Math.max(max,wL)
+        
+        }else{
+            hm[s[l]]--
+            l++
+        }
+    }
+   return max
 };
+
+// Time conmplexity => O(n^2) => old solution comments 
+// Second try => O(n) => only one pass
+// space complexity => O(n)
 
 
 const s = "ABAB"
